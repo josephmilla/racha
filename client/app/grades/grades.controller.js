fefc161156500329844e215684bd97e408fd5a87
Joseph Milla <jjtmilla@gmail.com>
2015-05-06 08:31:41 -0500

    Add grades

--- client/app/grades/grades.controller.js
+++ client/app/grades/grades.controller.js
@@ -28,16 +28,23 @@ angular.module('rachaApp')
     var item = {
       face: '/img/list/60.jpeg',
       what: 'Brunch this weekend?',
-      who: 'Min Li Chan',
+      grade: 'Min Li Chan',
       notes: "I'll be in your neighborhood doing errands."
     };
-    $scope.todos = [];
-    for (var i = 0; i < 15; i++) {
-      $scope.todos.push({
+
+    var classes = ['BADM 451', 'BADM 458', 'CS 125', 'CS 173', 'CWL 251', 'ACCY 200', 'ASTR 121', 'BADM 351', 'BUS 101', 'BUS 199', 'BTW 250', 'MATH 220', 'RHET 105', 'SOC 267', 'CS 465'];
+
+    var grades = ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'D+', 'D', 'D-', 'F'];
+
+    $scope.class_list = [];
+    for (var i = 0; i < 5; i++) {
+      var size = Math.floor((Math.random() * 30) + 5);
+
+      $scope.class_list.push({
         face: '/img/list/60.jpeg',
-        what: "Brunch this weekend?",
-        who: "Min Li Chan",
-        notes: "I'll be in your neighborhood doing errands."
+        what: classes[Math.floor((Math.random() * 15) + 0)],
+        grade: grades[Math.floor((Math.random() * 13) + 0)],
+        notes: Math.floor((Math.random() * size) + 0) + "/" + size
       });
     }
 
