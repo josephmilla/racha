2c188168a3542400089c31b302def5091aabf8ed
Joseph Milla <jjtmilla@gmail.com>
2015-05-10 17:14:17 -0500

    Fix dialog boxes

--- client/app/queues_class/queues_class.controller.js
+++ client/app/queues_class/queues_class.controller.js
@@ -72,6 +72,7 @@ angular.module('rachaApp')
       $scope.todos = [];
       for (var i = 0; i < 15; i++) {
         $scope.todos.push({
+          user: data.results[i].user,
           face: data.results[i].user.picture.thumbnail,
           what: classes[Math.floor((Math.random() * 15) + 0)],
           who: data.results[i].user.name.first.capitalizeFirstLetter()  + " " + data.results[i].user.name.last.capitalizeFirstLetter(),
@@ -85,23 +86,139 @@ angular.module('rachaApp')
       console.log("GET request failure");
     });
 
-    $scope.callTA = function(ev) {
-      // Appending dialog to document.body to cover sidenav in docs app
-      var confirm = $mdDialog.alert()
-        .parent(angular.element(document.body))
-        .title('Teaching Assistant')
-        .content('View Profile | Favorite | Cancel')
-        .ariaLabel('Lucky day')
-        .ok('OK')
-        .targetEvent(ev);
-      $mdDialog.show(confirm).then(function() {
-        $scope.alert = 'You decided to get rid of your debt.';
-      }, function() {
-        $scope.alert = 'You decided to keep your debt.';
+    // $scope.callTA = function(ev) {
+    //   // Appending dialog to document.body to cover sidenav in docs app
+    //   var confirm = $mdDialog.alert()
+    //     .parent(angular.element(document.body))
+    //     .title('Teaching Assistant')
+    //     .content('View Profile | Favorite | Cancel')
+    //     .ariaLabel('Lucky day')
+    //     .ok('OK')
+    //     .targetEvent(ev);
+    //   $mdDialog.show(confirm).then(function() {
+    //     $scope.alert = 'You decided to get rid of your debt.';
+    //   }, function() {
+    //     $scope.alert = 'You decided to keep your debt.';
+    //   });
+    // };
+
+    $scope.callTA = function(user) {
+      console.log(user);
+      $mdDialog.show({
+        controller: callTAController,
+        // templateUrl: 'dialog1.tmpl.html',
+        template:
+        '<md-dialog aria-label="List dialog" class="md-primary" layout-padding>' +
+           '  <md-dialog-content layout-padding>'+
+                '<h4>Teaching Assistant</h4>'+
+                '<span><span ng-click="joinQueue()" style="color: #337ab7;">Join Queue</span> | <span ng-click="viewProfile()" style="color: #337ab7;">View Profile</span> | <span ng-click="favorite()" style="color: #337ab7;">Favorite</span>'+
+           '  </md-dialog-content>' +
+           '  <div class="md-actions">' +
+           '    <md-button ng-click="cancelDialog()" class="md-primary">' +
+           '      Cancel' +
+           '    </md-button>' +
+           '    <md-button ng-click="closeDialog()" class="md-primary">' +
+           '      Ok' +
+           '    </md-button>' +
+           '  </div>' +
+           '</md-dialog>',
+        // targetEvent: ev
       });
+
+      function callTAController($scope, $mdDialog) {
+        $scope.cancelDialog = function() {
+          $mdDialog.hide();
+        }
+        $scope.closeDialog = function() {
+          $mdDialog.hide();
+        }
+        $scope.joinQueue = function() {
+          // Appending dialog to document.body to cover sidenav in docs app
+          // Modal dialogs should fully cover application
+          // to prevent interaction outside of dialog
+          $mdDialog.show(
+            $mdDialog.alert()
+              .parent(angular.element(document.body))
+              .title('Congratulations!')
+              .content('You have joined the queue')
+              .ariaLabel('Alert Dialog Demo')
+              .ok('Ok')
+              // .targetEvent(ev)
+          );
+        };
+        $scope.viewProfile = function() {
+          // Appending dialog to document.body to cover sidenav in docs app
+          // Modal dialogs should fully cover application
+          // to prevent interaction outside of dialog
+          // $mdDialog.show(
+          //   $mdDialog.alert()
+          //     .parent(angular.element(document.body))
+          //     .title('Congratulations!')
+          //     .content('You have joined the profile')
+          //     .ariaLabel('Alert Dialog Demo')
+          //     .ok('Ok')
+          //     .targetEvent(ev)
+          // );
+          console.log(user.name);
+          $mdDialog.show({
+            // templateUrl: 'dialog1.tmpl.html',
+            controller: callTAController,
+            template:
+            '<md-dialog aria-label="List dialog" class="md-primary" layout-padding>' +
+               '  <md-dialog-content layout-padding>'+
+                    '<h4>Teaching Assistant Profile</h4>'+
+                    '<img ng-src="' +  user.picture.medium + '" class="md-avatar" style="width: 100%;"><br><br>' +
+                    '<p>Name: ' + user.name.first.capitalizeFirstLetter() + " " + user.name.last.capitalizeFirstLetter() + '</p>' +
+                    '<p>Email: ' + user.email + '</p>'+
+                    '<p>Phone: ' + user.cell + '</p>'+
+               '  </md-dialog-content>' +
+               '  <div class="md-actions">' +
+               '    <md-button ng-click="cancelDialog()" class="md-primary">' +
+               '      Cancel' +
+               '    </md-button>' +
+               '    <md-button ng-click="closeDialog()" class="md-primary">' +
+               '      Ok' +
+               '    </md-button>' +
+               '  </div>' +
+               '</md-dialog>',
+            // targetEvent: ev
+          });
+        };
+        $scope.favorite = function() {
+          // Appending dialog to document.body to cover sidenav in docs app
+          // Modal dialogs should fully cover application
+          // to prevent interaction outside of dialog
+          $mdDialog.show(
+            $mdDialog.alert()
+              .parent(angular.element(document.body))
+              .title('Congratulations!')
+              .content('You have favorited your TA')
+              .ariaLabel('Alert Dialog Demo')
+              .ok('Ok')
+              // .targetEvent(ev)
+          );
+        };
+      }
     };
 
-    $scope.cancelTA = function(ev) {
+    // $scope.cancelTA = function(ev) {
+    //   // Appending dialog to document.body to cover sidenav in docs app
+    //   var confirm = $mdDialog.confirm()
+    //     .parent(angular.element(document.body))
+    //     .title('Cancel TA Appointment')
+    //     .content('Are you sure you want to cancel?')
+    //     .ariaLabel('Lucky day')
+    //     .ok('OK')
+    //     .cancel('Cancel')
+    //     .targetEvent(ev);
+    //   $mdDialog.show(confirm).then(function() {
+    //     $scope.alert = 'You decided to get rid of your debt.';
+    //   }, function() {
+    //     $scope.alert = 'You decided to keep your debt.';
+    //   });
+    // };
+
+    $scope.cancelTA = function(array, index) {
       // Appending dialog to document.body to cover sidenav in docs app
       var confirm = $mdDialog.confirm()
         .parent(angular.element(document.body))
@@ -110,9 +227,11 @@ angular.module('rachaApp')
         .ariaLabel('Lucky day')
         .ok('OK')
         .cancel('Cancel')
-        .targetEvent(ev);
+        // .targetEvent(ev);
       $mdDialog.show(confirm).then(function() {
         $scope.alert = 'You decided to get rid of your debt.';
+        array.splice(index, 1);
+        // alert("Removed");
       }, function() {
         $scope.alert = 'You decided to keep your debt.';
       });
